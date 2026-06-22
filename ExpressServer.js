8ea8342a044b5204ee2ab5ce8a5f35408853ada3
Afnan-Sultan-Trino <asultantrino@gmail.com>
2026-06-22 23:03:49 +0600

    Creating Express Server

---
+++ ExpressServer.js
@@ -0,0 +1,10 @@
+
+// Lesson 1 : (Create Express Server )
+
+// Express.js is an external module/package, so we have to initialize and install it
+// for initialize, we use -> npm init -y
+// for install, we use -> npm install express
+// we can also install nodemon package for browsing reload issues
+
+
+
