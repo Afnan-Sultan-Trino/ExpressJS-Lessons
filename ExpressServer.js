b3cfc9a57d40314d4fffe522f57198cfc3f6a28d
Afnan-Sultan-Trino <asultantrino@gmail.com>
2026-06-22 23:35:00 +0600

    Server Creation

--- ExpressServer.js
+++ ExpressServer.js
@@ -6,5 +6,15 @@
 // for install, we use -> npm install express
 // we can also install nodemon package for browsing reload issues
 
+// usually, this file should be as simple as that, and we create app.js for other task like method (GET,POST), Routing etc
 
 
+
+const app=require('./app')   // We need to import the app.js file here
+
+
+const PORT=3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
